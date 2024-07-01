6dc2599644412e18ff4f08e52850a5a0e426e671
Cloud <121166658+cloudedviper@users.noreply.github.com>
2024-07-01 04:42:32 -0500

    reduce

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const numbers = [1, 2, 3, 4, 5, 6];
 //const dubnums = numbers.map(num => num * 2);
 //console.log(dubnums)
 
-//const even = numbers.filter(num => num % 2 === 0)
+//const even = numbers.filter(num => num % 2 === 0);
 //console.log(even)
 
-//const found = numbers.find(num => num === 6)
+//const found = numbers.find(num => num === 6);
 //console.log(found)
 
+//const sum = numbers.reduce((acc, num) => (acc + num));
+//console.log(sum)
+
+//const odds = numbers.filter(num => num % 3 === 0);
+//console.log(odds)
+
 const sum = numbers.reduce((acc, num) => (acc + num));
-console.log(sum)
-
-
-
-
+console.log(sum);
 
